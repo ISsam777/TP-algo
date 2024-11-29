e65001543a6e92f120ad905d99a6ce9f90e45ebd
ISsam777 <119487532+ISsam777@users.noreply.github.com>
2024-11-29 18:02:27 +0100

    Update tp1-2.c

--- C_files/tp1-2.c
+++ C_files/tp1-2.c
@@ -1,33 +1,60 @@
+#include <stdio.h>
+int main() {
 
-#include<stdio.h>
-int main()
-{
-   int n;
-   int M[100][100];
-   printf("donner n : ");
-   scanf(" %d", &n);
-   
-   for(int i = 0;i<=n;i++){
-     for(int j = 0;j<=n;j++){
-      scanf("%d",&M[i][j]);
-     }
-   }
-   printf("aficher la matrice: \n"); 
-   for(int i = 0;i<=n;i++){
-     for(int j = 0;j<=n;j++){
-      printf(" %d ",M[i][j]);
-     }
-     printf("\n");
-   }
-   printf("\nafficher la matrice apres echange: \n"); 
-   for(int i = 0;i<=n;i++){
-     for(int j = 0;j<=n;j++){
-      printf(" %d ",M[j][i]);
-     }
-     printf("\n");
-   }
+  int n;
+  int M[100][100];
+  int N[100][100];
+  printf("donner n : ");
+  scanf(" %d", &n);
 
+  for (int i = 0; i < n; i++) {
 
+    for (int j = 0; j < n; j++) {
 
-	return 0;
+      scanf("%d", &M[i][j]);
+    }
+  }
+  printf("aficher la matrice: \n");
+  for (int i = 0; i < n; i++) {
+
+    for (int j = 0; j < n; j++) {
+
+      printf(" %d ", M[i][j]);
+    }
+    printf("\n");
+  }
+
+  for (int i = 0; i < n; i++) {
+
+    for (int j = i + 1; j < n; j++) {
+      int tmp = M[i][j];
+      M[i][j] = M[j][i];
+      M[j][i] = tmp;
+    }
+
+    printf("\n");
+  }
+  /**/
+  /*for (int i = 0; i < n; i++) {*/
+  /**/
+  /*  for (int j = 0; j < n; j++) {*/
+  /*    M[i][j] = N[i][j];*/
+  /*  }*/
+  /**/
+  /*  printf("\n");*/
+  /*}*/
+
+  printf("\nafficher la matrice apres echange: \n");
+
+  for (int i = 0; i < n; i++) {
+
+    for (int j = 0; j < n; j++) {
+
+      printf(" %d ", M[i][j]);
+    }
+
+    printf("\n");
+  }
+
+  return 0;
 }
